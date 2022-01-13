bf63befe2d9b2fa9cecb7c7da212734c776a174b
muhammed erdem <erdem.muhammed1@outlook.com>
2022-01-13 18:27:44 +0100

    font family changed

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,7 +6,9 @@
       </div>
       <div id="homepage">
         <h1>SHARE</h1>
+        <h3>To connect people each other</h3>
               <div id="nav">
+                
                 <router-link to="/">Home</router-link> |
                 <router-link to="/about">About</router-link>
               </div>
@@ -17,8 +19,9 @@
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap');
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Open Sans', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
