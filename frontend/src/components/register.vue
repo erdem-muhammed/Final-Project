72eb9b7d0c7fde923fee63a3ea5b53cb273cd7a6
muhammed erdem <erdem.muhammed1@outlook.com>
2022-01-25 23:10:24 +0100

    added required

--- frontend/src/components/register.vue
+++ frontend/src/components/register.vue
@@ -2,11 +2,11 @@
     <div class="container">
         <form class="registerForm">
             <h1>Register</h1>
-            <input type="text"     placeholder="Name" v-model="reg_name">
-            <input type="text"     placeholder="Surname" v-model="reg_surname">
-            <input type="text"     placeholder="Email" v-model="reg_email">
-            <input type="password" placeholder="Password" v-model="reg_pass">
-            <input type="text"     placeholder="Phone Number" v-model="reg_phone">
+            <input type="text"     placeholder="Name" v-model="reg_name" required>
+            <input type="text"     placeholder="Surname" v-model="reg_surname" required>
+            <input type="text"     placeholder="Email" v-model="reg_email" required>
+            <input type="password" placeholder="Password" v-model="reg_pass" required>
+            <input type="text"     placeholder="Phone Number" v-model="reg_phone" required>
             <input type="submit"   value="Submit">
         </form>
     </div>
