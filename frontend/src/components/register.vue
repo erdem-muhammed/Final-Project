6c2267e8fbbe5187ca2797834c9f9560b86b6e15
muhammed erdem <erdem.muhammed1@outlook.com>
2022-01-26 14:53:19 +0100

    register()

--- frontend/src/components/register.vue
+++ frontend/src/components/register.vue
@@ -25,6 +25,8 @@ export default {
     },
     methods: {
         register(){
+            const formData = new FormData();
+            formData.append("name", this.reg_name);
             
         }
     }
