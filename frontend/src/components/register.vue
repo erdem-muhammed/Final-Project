74eb9fd68c058cd90884e1065c739dfb924bb6c1
muhammed erdem <erdem.muhammed1@outlook.com>
2022-01-26 20:29:33 +0100

    reg_info corrected

--- frontend/src/components/register.vue
+++ frontend/src/components/register.vue
@@ -7,6 +7,7 @@
             <input type="text"     placeholder="Email" v-model="reg_email" required>
             <input type="password" placeholder="Password" v-model="reg_pass" required>
             <input type="text"     placeholder="Phone Number" v-model="reg_phone" required>
+            <div>{{ reg_info }}</div>
             <input type="submit"   value="Submit" @click="register">
         </form>
     </div>
@@ -21,6 +22,7 @@ export default {
             reg_email: "",
             reg_pass: "",
             reg_phone: "",
+            reg_info: "",
             new_user: false
         };
     },
@@ -42,16 +44,14 @@ export default {
             .then(result => {
                 if(result.successfull == "1")
                 {
-                    this.new_user = true;
+                    this.new_user = false;
                 }
                 else
                 {
                     console.log("register failed!!");
                 }
-            })
-            .catch(function(error) {
-                    console.log(error);
             });
+          
         }
     }
     
