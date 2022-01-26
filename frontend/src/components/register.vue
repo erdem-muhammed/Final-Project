971ae350d1b72a6ca35f4cf5d654ec1aca1ba3c9
muhammed erdem <erdem.muhammed1@outlook.com>
2022-01-26 14:59:38 +0100

    method post

--- frontend/src/components/register.vue
+++ frontend/src/components/register.vue
@@ -31,7 +31,10 @@ export default {
             formData.append("email", this.reg_email);
             formData.append("password", this.reg_pass);
             formData.append("phone", this.reg_phone);
-            fetch('http://localhost/final_project/register.php')
+            fetch('http://localhost/final_project/register.php',{
+                method: 'post',
+                
+            })
         }
     }
     
