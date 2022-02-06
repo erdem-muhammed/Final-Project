d18d36630c583333c524ec6ecbc00ff94b33242a
muhammed erdem <erdem.muhammed1@outlook.com>
2022-02-06 15:45:08 +0100

    created post.vue

--- frontend/src/components/flow.vue
+++ frontend/src/components/flow.vue
@@ -1,12 +1,29 @@
 <template>
-    <div>
+    <div class="container">
+        <div class="post">
+                <Post />
+        </div>
+
+
 
     </div>
 </template>
 
 
 <script>
+import Post from "./post.vue";
 export default {
-    
+    components: {
+        Post
+
+    }
 }
 </script>
+
+
+
+<style>
+.container{
+    padding: 20px 30px;
+}
+</style>
